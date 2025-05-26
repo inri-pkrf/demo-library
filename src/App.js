9f5c2474683b280a2bf234d56ff6e9f3124c6c3b
Alma-yovell <alma.yovell@gmail.com>
2025-05-26 14:41:39 +0300

    ניסוי לשנות app בשביל ריפרש

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { HashRouter as Router, Route, Routes, useLocation } from 'react-router-dom'; 
-import Layout from './Layout'; 
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Layout from './Layout';
 import Home from './pages/Home';
 import Search from './pages/Search';
 import Simulation from './pages/Simulation';
 import AIEngines from './pages/AIEngines';
 import PromptGenerator from './pages/PromptGenerator';
 import Contact from './pages/Contact';
-import './App.css'; 
+import './App.css';
 
 function App() {
   const location = useLocation();
   const [currentPageName, setCurrentPageName] = useState('Home');
 
   useEffect(() => {
-    console.log('Initial location.pathname:', location.pathname); // הוספה
     const pathSegments = location.pathname.split('/').filter(Boolean);
-    let pageName = 'Home'; // Default to Home
+    let pageName = 'Home';
     if (pathSegments.length > 0) {
       pageName = pathSegments[0].charAt(0).toUpperCase() + pathSegments[0].slice(1);
     }
-    // Normalize specific names
     if (pageName.toLowerCase() === 'aiengines') pageName = 'AIEngines';
     if (pageName.toLowerCase() === 'promptgenerator') pageName = 'PromptGenerator';
 
